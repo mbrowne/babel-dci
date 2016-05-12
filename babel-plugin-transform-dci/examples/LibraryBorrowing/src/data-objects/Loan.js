be106cec5b29f125aecfd591fa67c757dca193ac
Matthew Browne <mbrowne83@gmail.com>
2016-05-11 23:58:15 -0400

    added startDate property to Loan object

--- babel-plugin-transform-dci/examples/LibraryBorrowing/src/data-objects/Loan.js
+++ babel-plugin-transform-dci/examples/LibraryBorrowing/src/data-objects/Loan.js
@@ -1,5 +1,6 @@
 export default function Loan(borrower: User, itemRecord: itemRecord, numDays: number) {
 	this._id = null; //This should be auto-generated somehow
+	this._startDate = new Date();
 	this._borrower = borrower;
 	this._itemRecord = itemRecord;
 	this._numDays = numDays;
@@ -8,6 +9,7 @@ export default function Loan(borrower: User, itemRecord: itemRecord, numDays: nu
 Loan.prototype = {
 	constructor: Loan,
 	get id(): number {return this._id},
+	get startDate(): Date {return this._startDate},
 	get borrower(): User {return this._borrower},
 	get itemRecord(): ItemRecord {return this._itemRecord},
 	get numDays(): number {return this._numDays}
