e9ed1134be107862ed31afa02ccde0b73a2424e1
Matthew Browne <mbrowne83@gmail.com>
2016-06-15 17:35:12 -0400

    removed outdated commented code

--- babel-plugin-transform-dci/examples/LibraryBorrowing/src/index.js
+++ babel-plugin-transform-dci/examples/LibraryBorrowing/src/index.js
@@ -1,14 +1,9 @@
 import "babel-polyfill";
 
-//import BorrowLibraryItems from './BorrowLibraryItems';
 import Controller from './Controller';
 import app from './app';
 import loadDemoData from './loadDemoData';
 
 loadDemoData();
 
-//session.user = new User(12345, 'Test', 'User');
-
 new Controller().init();
-
-//BorrowLibraryItems(new Controller());
